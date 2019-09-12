14f08f314b5727f136c38727f666504132b491e1
michal0127 <michal.rebis@interia.pl>
2019-09-12 08:43:51 +0200

    dodanie pierwszych widoków

---
+++ pizza/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('komunikat/', views.komunikat, name='komunikat'),
+    path('napoj/', views.napoj, name='napoj'),
+]
