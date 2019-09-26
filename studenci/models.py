2715f13731b7e99e538c196aa85c219e0dbc67eb
michal0127 <michal.rebis@interia.pl>
2019-09-26 08:44:35 +0200

    gfsasgf

---
+++ studenci/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Miasto(models.Model):
+    nazwa = models.CharField(
+        verbose_name='miasto',
+        max_length=30,
+        help_text='Nazwa miasta')
+    kod = models.CharField(
+        max_length=6,
+        help_text='Kod pocztowy')
+
+class Uczelnia(models.Model):
+    nazwa = models.CharField(
+        verbose_name='uczelnia',
+        max_length=30,
+        help_text='Nazwa uczelni')
+
+class Student(models.Model):
+    imie = models.CharField('imię', max_length=30)
+    nazwisko = models.CharField(max_length=30)
+    uczelnia = models.ForeignKey(Uczelnia, on_delete=models.SET_NULL, null=True)
+    miasto = models.ForeignKey(Miasto, on_delete=models.SET_NULL, null=True)
+    roks = models.CharField('rok studiów', max_length=3)
+    dochod = models.DecimalField(max_digits='6', decimal_places=2)
